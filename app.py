dba3dbf2a826468356dce8b207d76b7917fcedc6
animndvl <mjngrl@gmail.com>
2025-10-24 03:41:28 -0700

    actualizacion de app.py y listar_notas, para eliminar nota

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 from nota import ManejadorDeNotas
 
 app = Flask(__name__)
-manejador = ManejadorDeNotas()
 
 @app.route('/')
 @app.route('/index')
@@ -26,5 +25,41 @@ def listar_notas():
     manejador.cerrar()
     return render_template('listar_notas.html', notas=notas)
 
+@app.route('/modificar_nota/<int:id>', methods=['GET', 'POST'])
+def modificar_nota(id):
+    manejador = ManejadorDeNotas()
+    nota = next((n for n in manejador.leer_notas() if n.id == id), None)
+    if not nota:
+        manejador.cerrar()
+        return "Nota no encontrada", 404
+
+    if request.method == 'POST':
+        nuevo_titulo = request.form['titulo']
+        nuevo_contenido = request.form['contenido']
+        manejador.actualizar_nota(id, nuevo_titulo, nuevo_contenido)
+        manejador.cerrar()
+        return redirect(url_for('listar_notas'))
+
+    manejador.cerrar()
+    return render_template('modificar_nota.html', nota=nota)
+
+
+@app.route('/eliminar_nota/<int:id>', methods=['GET', 'POST'])
+def eliminar_nota(id):
+    manejador = ManejadorDeNotas()
+
+    nota = next((n for n in manejador.leer_notas() if n.id == id), None)
+    if not nota:
+        manejador.cerrar()
+        return "Nota no encontrada", 404
+    
+    if request.method == 'POST':
+        manejador.eliminar_nota(id)
+        manejador.cerrar()
+        return redirect(url_for('listar_notas'))
+    
+    manejador.cerrar()
+    return render_template('eliminar_nota.html', nota=nota)
+    
 if __name__ == '__main__':
     app.run(debug=True)
